52fb951bbf869c1bb4e936e1d31400b8fee89c45
alejandrin09 <zs21013870@estudiantes.uv.mx>
2024-06-22 23:53:55 -0600

    Se agrego funcionalidad de generación de reportes

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,15 +1,22 @@
-using System.Security.Claims;
-using closirissystem.Models;
+using AspNetCore.Reporting;
 using closirissystem.Services;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace closirissystem;
 
-public class AdminController(UserClientService user) : Controller
+public class AdminController : Controller
 {
+    private readonly UserClientService _user;
+    private readonly IWebHostEnvironment? _webHostEnvironment;
+
+    public AdminController(UserClientService user, IWebHostEnvironment webHostEnvironment)
+    {
+        _user = user;
+        _webHostEnvironment = webHostEnvironment;
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+    }
+
     [AllowAnonymous]
     public IActionResult Index()
     {
@@ -20,7 +27,7 @@ public class AdminController(UserClientService user) : Controller
     [Authorize(Roles = "Administrador")]
     public async Task<IActionResult> Users()
     {
-        var users = await user.GetUsersAsync();
+        var users = await _user.GetUsersAsync();
         return PartialView("_Users", users);
     }
 
@@ -28,7 +35,52 @@ public class AdminController(UserClientService user) : Controller
     [Authorize(Roles = "Administrador")]
     public async Task<IActionResult> Audit()
     {
-        var audit = await user.GetLogbooksAsync();
+        var audit = await _user.GetLogbooksAsync();
         return PartialView("_Audit", audit);
+    }
+
+    public async Task<IActionResult> UsersReport()
+    {
+
+        var path = Path.Combine(_webHostEnvironment.WebRootPath, "reports", "Users.rdlc");
+        Dictionary<string, string> parameters = new Dictionary<string, string>
+            {
+                { "prm", "Users Report" }
+            };
+
+        var localReport = new LocalReport(path);
+        var users = await _user.GetUsersAsync();
+        localReport.AddDataSource("DataSetUsers", users);
+
+        var result = localReport.Execute(RenderType.Pdf, 1, parameters);
+
+        string base64String = Convert.ToBase64String(result.MainStream);
+
+        ViewBag.PdfBase64Users = base64String;
+
+        return PartialView("_UsersReport");
+
+    }
+
+    public async Task<IActionResult> AuditReport()
+    {
+        var path = Path.Combine(_webHostEnvironment.WebRootPath, "reports", "Audit.rdlc");
+        Dictionary<string, string> parameters = new Dictionary<string, string>
+            {
+                { "prm", "Audit Report" }
+            };
+
+        var localReport = new LocalReport(path);
+        var audit = await _user.GetLogbooksAsync();
+        localReport.AddDataSource("DataSetAudit", audit);
+
+        var result = localReport.Execute(RenderType.Pdf, 1, parameters);
+
+        string base64String = Convert.ToBase64String(result.MainStream);
+
+        ViewBag.PdfBase64Audit = base64String;
+
+        return PartialView("_AuditReport");
+
     }
 }
